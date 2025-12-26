b3430b678255841ea237c80bbb06784446d72167
_Avvake_ <denisergo18@gmail.com>
2025-12-26 16:51:53 +0200

    created docker-compose for testing

--- crm_backend/tests/integration/test_user.py
+++ crm_backend/tests/integration/test_user.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.mark.asyncio
 class TestUser:
-    async def test_register_user(self):
+    async def test_register_user(self, async_client, db_session):
         assert 1 == 1
